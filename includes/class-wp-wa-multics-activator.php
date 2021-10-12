37ca100a82ff6ef6c18522bd348fecb136b92a3d
agusnurwanto <agusnurwantomuslim@gmail.com>
2021-10-12 20:48:52 +0700

    initial commit

---
+++ includes/class-wp-wa-multics-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/agusnurwanto
+ * @since      1.0.0
+ *
+ * @package    Wp_Wa_Multics
+ * @subpackage Wp_Wa_Multics/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Wa_Multics
+ * @subpackage Wp_Wa_Multics/includes
+ * @author     Agus Nurwanto <agusnurwantomuslim@gmail.com>
+ */
+class Wp_Wa_Multics_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-wa-multics-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/agusnurwanto
+ * @since      1.0.0
+ *
+ * @package    Wp_Wa_Multics
+ * @subpackage Wp_Wa_Multics/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Wa_Multics
+ * @subpackage Wp_Wa_Multics/includes
+ * @author     Agus Nurwanto <agusnurwantomuslim@gmail.com>
+ */
+class Wp_Wa_Multics_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
